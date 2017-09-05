74e45b6ad45f0c1bcdc09f8b5017415dedce4f92
Stefan Neidig <s.neidig@appcom-interactive.de>
2017-09-05 09:23:28 +0200

    Add butterknife to project

--- app/src/main/java/de/appcom/pokedexapp4/activities/MainActivity.java
+++ app/src/main/java/de/appcom/pokedexapp4/activities/MainActivity.java
@@ -1,16 +1,23 @@
 package de.appcom.pokedexapp4.activities;
 
 import android.app.Application;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.OnClick;
 import de.appcom.pokedexapp4.R;
 import de.appcom.pokedexapp4.annotations.Layout;
+import org.w3c.dom.Text;
 import timber.log.Timber;
 
 @Layout(R.layout.activity_main) public class MainActivity extends BaseActivity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    Timber.d("Hey this is a log message");
+  @BindView(R.id.textView) TextView textview;
+
+  @OnClick(R.id.textView) public void onTextViewClicked(View view) {
+    ((TextView) view).setText("Oy you clicked me");
   }
 }
