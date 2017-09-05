97672d79d8653384c923b5d421397c26b6520ade
Stefan Neidig <s.neidig@appcom-interactive.de>
2017-09-05 09:16:01 +0200

    Add timber to project

--- app/src/main/java/de/appcom/pokedexapp4/PokedexApplication.java
+++ app/src/main/java/de/appcom/pokedexapp4/PokedexApplication.java
@@ -1,10 +1,28 @@
 package de.appcom.pokedexapp4;
 
 import android.app.Application;
+import timber.log.Timber;
 
 /**
  * @author Stefan Neidig
  */
 
 public class PokedexApplication extends Application {
+
+  @Override public void onCreate() {
+    super.onCreate();
+
+    setupTimber();
+  }
+
+  private void setupTimber() {
+    if (BuildConfig.DEBUG) {
+      Timber.plant(new Timber.DebugTree() {
+        @Override protected String createStackElementTag(StackTraceElement element) {
+          return super.createStackElementTag(element) + "," + element.getFileName() + ", " + element.getMethodName()
+              + ", " + element.getLineNumber();
+        }
+      });
+    }
+  }
 }
--- app/src/main/java/de/appcom/pokedexapp4/activities/MainActivity.java
+++ app/src/main/java/de/appcom/pokedexapp4/activities/MainActivity.java
@@ -1,12 +1,16 @@
 package de.appcom.pokedexapp4.activities;
 
+import android.app.Application;
 import android.os.Bundle;
+import android.widget.TextView;
 import de.appcom.pokedexapp4.R;
 import de.appcom.pokedexapp4.annotations.Layout;
+import timber.log.Timber;
 
 @Layout(R.layout.activity_main) public class MainActivity extends BaseActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Timber.d("Hey this is a log message");
   }
 }
