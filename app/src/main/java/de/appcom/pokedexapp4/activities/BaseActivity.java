d23b4c16425ca764598197e3cc2b82eeeee6c32d
Stefan Neidig <s.neidig@appcom-interactive.de>
2017-09-05 09:23:28 +0200

    Add butterknife to project

--- app/src/main/java/de/appcom/pokedexapp4/activities/BaseActivity.java
+++ app/src/main/java/de/appcom/pokedexapp4/activities/BaseActivity.java
@@ -2,8 +2,12 @@ package de.appcom.pokedexapp4.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import de.appcom.pokedexapp4.annotations.Layout;
 import java.lang.annotation.Annotation;
+
+import static butterknife.ButterKnife.bind;
 
 /**
  * @author Stefan Neidig
@@ -11,9 +15,18 @@ import java.lang.annotation.Annotation;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
+  Unbinder unbinder;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(getLayoutResId());
+
+    unbinder = ButterKnife.bind(this);
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    unbinder.unbind();
   }
 
   private int getLayoutResId() {
